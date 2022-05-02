a5ceacaf27f7011d4ba715282d8b0b657dde441e
mahesh gawande <maheshsg297@gmail.com>
2022-05-02 13:45:50 +0530

    1. Driver close implemented 2. Updated error messages for log

--- ReadyDriver.py
+++ ReadyDriver.py
@@ -9,28 +9,37 @@ class GetDriver:
     def open_driver(self):
         driver = False
         err_msg = ""
-        if(self.check_path()):
+        path_exists = self.check_path()
+
+        if(path_exists):
             d = webdriver.Chrome(self.path)
-            err_msg = "ReadyDriver -> GetDriver -> open_driver -> Driver initialized!"
+            err_msg = "ReadyDriver.py-->GetDriver-->open_driver--> Driver initialized successfully!"
             driver = d
         else:
-            err_msg = "ReadyDriver -> GetDriver -> open_driver -> Failed to open driver!"
+            err_msg = "ReadyDriver.py-->GetDriver-->open_driver--> Failed to open driver!"
             driver = False
         
         CreateLog(err_msg)
         return driver
 
     def close_driver(self):
-        get_status(self.driver)
-        if():
-            pass
-        else:
+        try:
+            self.driver.quit()
+            err_msg = "ReadyDriver.py-->GetDriver-->close_driver--> Driver closed successfully!"
+        except Exception as ex:
+            err_msg = f"ReadyDriver.py-->GetDriver-->close_driver--> {type(ex).__name__}"
             pass
 
+        CreateLog(err_msg)
+
     def check_path(self):
+        ret = False
         if(exists(self.path)):
-            return True
+            err_msg = "ReadyDriver.py-->GetDriver-->check_path--> Driver file exists!"
+            ret = True
         else:
-            err_msg = "ReadyDriver -> GetDriver -> check_path -> Driver file does bot exists!"
-            CreateLog(err_msg)
-            return False
+            err_msg = "ReadyDriver.py-->GetDriver-->check_path--> Driver file does not exists!"
+            ret = False
+
+        CreateLog(err_msg)
+        return ret
